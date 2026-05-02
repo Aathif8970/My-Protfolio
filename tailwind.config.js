356496425c20af1dfe8fc28b8660a7bf6705de2e
Mohamed Aathif <mohamedaathif8970@gmail.com>
2026-05-02 19:46:05 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+tailwind.congig ={
+    theme:{
+        extends:{
+            gridTemplateColumns:{
+            'auto':'repeat(auto-fit, minmax(200px, 1fr))'
+        },
+        fontFamily:{
+            Outfit:["Outfit", "sans-serif"],
+            Ovo:[ "Ovo", "serif"]
+        },
+        colors:{
+            lightHover: '#fcf4ff',
+            darkHover: '#fcf4ff',
+            darkTheme:'#11001F'
+        }
+    }
+
+        },
+        
+        darkMode:'selector'
+        
+}
+
